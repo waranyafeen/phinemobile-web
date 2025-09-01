f082b805792c8495f08a5a7e1c4b195bebbdf290
waranyafeen <144030485+waranyafeen@users.noreply.github.com>
2025-09-01 21:51:38 +0700

    create sidebar-page

--- src/app/backoffice/sidebar.tsx
+++ src/app/backoffice/sidebar.tsx
@@ -1,4 +1,4 @@
-
+import Link from "next/link";
 
 export default function Sidebar() {
     return (
@@ -8,9 +8,42 @@ export default function Sidebar() {
             </div>
 
             <div className="flex flex-col gap-3 text-white text-xl p-5">
-                <div>Dashboard</div>
-                <div>Company</div>
-                <div>User</div>
+                <div>
+                    <Link href="/backoffice/dashboard">
+                        <i className="fa fa-tachometer-alt mr-2 w-[25px] text-center"></i>
+                        Dashboard
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/backoffice/buy">
+                        <i className="fa fa-shopping-cart mr-2 w-[25px] text-center"></i>
+                        ซื้อสินค้า
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/backoffice/sell">
+                        <i className="fa fa-dollar-sign mr-2 w-[25px] text-center"></i>
+                        ขายสินค้า
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/backoffice/repair">
+                        <i className="fa fa-tools mr-2 w-[25px] text-center"></i>
+                        รับซ่อมสินค้า
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/backoffice/company">
+                        <i className="fa fa-building mr-2 w-[25px] text-center"></i>
+                        ข้อมูลร้าน
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/backoffice/user">
+                        <i className="fa fa-user mr-2 w-[25px] text-center"></i>
+                        ข้อมูลผู้ใช้งานระบบ
+                    </Link>
+                </div>
             </div>
         </div>
     );
