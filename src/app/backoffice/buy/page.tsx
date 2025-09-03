c049545779ce55ac23ef1f7f4cc8ad30b379c6b4
waranyafeen <144030485+waranyafeen@users.noreply.github.com>
2025-09-03 22:51:05 +0700

    update page buy web

--- src/app/backoffice/buy/page.tsx
+++ src/app/backoffice/buy/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios  from "axios";
 import { config } from "@/app/config";
 import Swal from "sweetalert2";
@@ -18,6 +18,26 @@ export default function Page() {
     const [customerPhone, setCustomerPhone] = useState('');
     const [customerAddress, setCustomerAddress] = useState('');
     const [remark, setRemark] = useState('');
+    const [products, setProducts] = useState([]); //สินค้าที่ซื้อ
+    const [id, setId] = useState(0); //สำหรับแก้ไขข้อมูล
+    const [qty, setQty] = useState(1);  //จำนวนสินค้า
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const res = await axios.get(`${config.apiUrl}/buy/list`);
+            setProducts(res.data);
+        } catch (err: any) {
+            Swal.fire({
+                icon: 'error',
+                title: 'เกิดข้อผิดพลาด',
+                text: err.message
+            });
+        }
+    };
 
     const handleOpenModal = () => {
         setIsOpen(true);
@@ -38,17 +58,28 @@ export default function Page() {
                 customerName: customerName,
                 customerPhone: customerPhone,
                 customerAddress: customerAddress,
-                remark: remark
+                remark: remark,
+                qty: qty
             }
 
-            //ส่งค่าไปหลังบ้าน
-            const res = await axios.post(`${config.apiUrl}/buy/create`, payload);
+            if (id === 0) {
+                // ส่งค่าไปสร้างข้อมูลใหม่
+                await axios.post(`${config.apiUrl}/buy/create`, payload);
+            } else {
+                // ส่งค่าไปแก้ไขข้อมูล
+                await axios.put(`${config.apiUrl}/buy/update/${id}`, payload);
+                setId(0); //รีเซ็ต id ให้เป็น 0 เพื่อเตรียมเพิ่มข้อมูลใหม่
+            }
+
             Swal.fire({
                 icon: 'success',
                 title: 'บันทึกข้อมูลสำเร็จ',
                 text: 'บันทึกเรียบร้อยแล้ว',
                 timer: 2000
             });
+
+            handleCloseModal();
+            fetchData(); //รีเฟรชข้อมูลใหม่
 
         } catch (error) {
             Swal.fire({
@@ -59,14 +90,108 @@ export default function Page() {
         }
     }
 
+    // แก้ไขข้อมูล ดึงข้อมูลมาแสดงในฟอร์ม
+    const handleEdit = (id: number) => {
+        const product = products.find((p: any) => p.id === id) as any;
+        setSerial(product.serial ?? '');
+        setName(product.name);
+        setRelease(product.release);
+        setColor(product.color);
+        setPrice(product.price);
+        setCustomerName(product.customerName);
+        setCustomerPhone(product.customerPhone);
+        setCustomerAddress(product.customerAddress ?? '');
+        setRemark(product.remark ?? '');
+        setId(product.id);
+        handleOpenModal();
+    }
+
+    const handleDelete = async (id: number) => {
+        try {
+            const button = await Swal.fire({
+                title: 'คุณต้องการลบข้อมูลนี้ใช่หรือไม่', 
+                icon: 'warning',
+                showCancelButton: true,
+                showConfirmButton: true,
+                confirmButtonText: 'ลบ',
+                cancelButtonText: 'ยกเลิก'
+            });
+
+            if (button.isConfirmed) {
+                await axios.delete(`${config.apiUrl}/buy/remove/${id}`);
+                fetchData();
+            }
+        } catch (error: any) {
+            Swal.fire({
+                icon: 'error',
+                title: 'เกิดข้อผิดพลาด',
+                text: error.message
+            });
+        }
+    }
+
+    const handleClear = () => {
+        setSerial('');
+        setName('');
+        setRelease('');
+        setColor('');
+        setPrice(0);
+        setCustomerName('');
+        setCustomerPhone('');
+        setCustomerAddress('');
+        setRemark('');
+        setQty(1); //กันบัค
+    }   
+
     return (
         <div>
             <h1 className="content-header">รายการซื้อ</h1>
+
             <div>
-                <button className="btn" onClick={handleOpenModal}>
+                <button className="btn" onClick={() => {handleClear(); handleOpenModal();}}>
                     <i className="fa-solid fa-plus mr-2"></i>เพิ่มรายการ
                 </button> 
+                <table className="table mt-3">
+                    <thead>
+                        <tr>
+                            <th>Serial</th>
+                            <th>ชื่อสินค้า</th>
+                            <th>รุ่น</th>
+                            <th>สี</th>
+                            <th>ราคา</th>
+                            <th>ชื่อผู้ซื้อ</th>
+                            <th>เบอร์โทรศัพท์ลูกค้า</th>
+                            <th>ที่อยู่ลูกค้า</th>
+                            <th>หมายเหตุ</th>
+                            <th className="w-[110px] "></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map((product: any) => (
+                            <tr key={product.id}>
+                                <td>{product.serial}</td>
+                                <td>{product.name}</td>
+                                <td>{product.release}</td>
+                                <td>{product.color}</td>
+                                <td>{product.price}</td>
+                                <td>{product.customerName}</td>
+                                <td>{product.customerPhone}</td>
+                                <td>{product.customerAddress}</td>
+                                <td>{product.remark}</td>
+                                <td className="text-center">
+                                    <button className="btn-edit mr-1" onClick={() => handleEdit(product.id)}>
+                                        <i className="fa-solid fa-edit"></i>
+                                    </button>
+                                    <button className="btn-delete mr-1" onClick={() => handleDelete(product.id)}>
+                                        <i className="fa-solid fa-trash"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
+
             <Modal title="เพิ่มรายการ" isOpen={isOpen} onClose={handleCloseModal}>
                 <div>serial สินค้า</div>
                 <input type="text" value={serial} onChange={(e) => setSerial(e.target.value)} />
@@ -94,6 +219,9 @@ export default function Page() {
 
                 <div>หมายเหตุ</div>
                 <input type="text" value={remark} onChange={(e) => setRemark(e.target.value)} />
+
+                <div>จำนวนสินค้า</div>
+                <input type="number" value={qty} onChange={(e) => setQty(Number(e.target.value ?? 0))} />
 
                 <div>
                     <button className="btn" onClick={handleSave}>
